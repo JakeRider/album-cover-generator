602bc43f45670769c48ab9b2a5cb8a1502630c80
Jake Rider <hello@jakerider.io>
2019-04-15 16:05:31 -0400

    Style the frontend

--- covergen/album/helpers.py
+++ covergen/album/helpers.py
@@ -9,6 +9,6 @@ from .wordlists import animals, colors, cottons, weathers
 def generate_random_album() -> Album:
     """Generate a random album."""
     title = f'{choice(weathers)} {choice(cottons)}'.title()
-    artist = f'{choice(colors)} {choice(animals)}'.title()
+    artist = f'by {choice(colors)} {choice(animals)}'.title()
 
     return Album(title, artist)
