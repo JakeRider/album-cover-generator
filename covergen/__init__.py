b83aaaaaedf7905e338e0569197446304351f110
Jake Rider <hello@jakerider.io>
2019-04-15 14:46:54 -0400

    Generate static album cover

--- covergen/__init__.py
+++ covergen/__init__.py
@@ -1,6 +1,16 @@
 """The main entry point of the application."""
 
+from dataclasses import dataclass
+
 from flask import Flask, render_template
+
+
+@dataclass
+class Album:
+    """A dataclass representing an album."""
+
+    title: str
+    artist: str
 
 
 def create_app() -> Flask:
@@ -8,7 +18,9 @@ def create_app() -> Flask:
     app = Flask(__name__)
 
     @app.route('/')
-    def say_hello() -> str:
-        return render_template('base.html')
+    def home() -> str:
+        album = Album('Chronicles', 'Rush')
+
+        return render_template('base.html', album=album)
 
     return app
