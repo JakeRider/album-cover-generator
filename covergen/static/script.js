5ecdc9b1e3fb6a3a089e8d05a602dc342e7c5dd9
Jake Rider <hello@jakerider.io>
2019-04-16 17:44:15 -0400

    Handle album text update

--- covergen/static/script.js
+++ covergen/static/script.js
@@ -1,10 +1,22 @@
-function fetchAlbumJSON() {
-  console.log('Fetching!');
+async function fetchAlbumJSON() {
+  const response = await fetch('/api/v1/album/generate-random');
+  const json = await response.json();
+
+  return json;
+}
+
+async function updateAlbum() {
+  albumTitle = document.querySelector('#albumTitle');
+  albumArtist = document.querySelector('#albumArtist');
+  albumData = await fetchAlbumJSON();
+
+  albumTitle.textContent = albumData.title;
+  albumArtist.textContent = albumData.artist;
 }
 
 function init() {
   newAlbumBtn = document.querySelector('#newAlbumBtn');
-  newAlbumBtn.addEventListener('click', fetchAlbumJSON);
+  newAlbumBtn.addEventListener('click', updateAlbum);
 }
 
 window.onload = init;
